680ae9a7414e5b579e4d6fb040fd5aed91838bd5
rubenm <rumabe97@gmail.com>
2021-12-28 20:50:46 +0100

    cryptoZombies lesson 5

---
+++ ZombieFactory.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8.11;
+import "./Ownable.sol";
+import "./SafeMath.sol";
+
+contract ZombieFactory is Ownable {
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+    uint32 level;
+    uint32 readyTime;
+    uint256 cooldownTime = 1 days;
+
+    Zombie[] public zombies;
+
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
+    }
+
+    function _createZombie(string memory _name, uint256 _dna) internal {
+        zombies.push(
+            Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0)
+        );
+        uint256 id = zombies.length - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encode(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
